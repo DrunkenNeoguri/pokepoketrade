68713311ea66ea92e00ac24d14024fcde524e063
developneoguri <developneoguri@gmail.com>
2025-03-31 22:07:37 +0900

    feat: Radio 컴포넌트 스타일링 및 기본 Props 적용

--- components/radio/index.tsx
+++ components/radio/index.tsx
@@ -1,12 +1,22 @@
 import {Pressable, Text, View} from 'react-native';
+import {radioStyle} from './style';
 
-export function Radio() {
+interface Props {
+  id: string;
+  isSelected: boolean;
+  onPress?: (id: string) => void;
+  children?: React.ReactNode;
+}
+
+export function Radio({id, isSelected, onPress, children}: Props) {
   return (
-    <Pressable>
-      <View>
-        <View />
+    <Pressable id={id} onPress={() => {}} style={radioStyle.basic}>
+      <View style={radioStyle.border}>
+        <View
+          style={isSelected ? radioStyle.selected : radioStyle.unselected}
+        />
       </View>
-      <Text>Radio</Text>
+      <Text style={radioStyle.label}>{children}</Text>
     </Pressable>
   );
 }
---
+++ components/radio/style.ts
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native';
+
+export const radioStyle = StyleSheet.create({
+  basic: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 4,
+  },
+  border: {
+    paddingVertical: 1,
+    paddingHorizontal: 1,
+    borderRadius: 9999,
+    boxSizing: 'border-box',
+    borderWidth: 1,
+    borderColor: '#373737',
+    backgroundColor: '#ffffff',
+    width: 12,
+    height: 12,
+  },
+  selected: {
+    width: 8,
+    height: 8,
+    borderRadius: 9999,
+    backgroundColor: '#5881FF',
+  },
+  unselected: {
+    borderRadius: 9999,
+    backgroundColor: '#ffffff',
+  },
+  label: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: 400,
+    color: '#373737',
+  },
+});
