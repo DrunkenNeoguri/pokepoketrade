bd81416f771a9c7d36ab0f490ddc3d44f0b57ab5
developneoguri <developneoguri@gmail.com>
2025-03-31 22:08:01 +0900

    feat: TradeItem 컴포넌트 스타일링 및 임시 Props 적용

--- components/trade-item/index.tsx
+++ components/trade-item/index.tsx
@@ -1,5 +1,29 @@
-import {Pressable} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
+import {tradeItemStyle} from './style';
+import {Badge} from '../badge';
 
-export function TradeItem() {
-  return <Pressable>TradeItem</Pressable>;
+interface Props {
+  id: string;
+  title: string;
+  card1: string;
+  card2: string;
+}
+
+export function TradeItem(props: Props) {
+  return (
+    <Pressable id={props.id} style={tradeItemStyle.basic}>
+      <View style={tradeItemStyle.titleBox}>
+        <Text style={tradeItemStyle.title}>{props.title}</Text>
+        <Badge type="교환" />
+      </View>
+      <View style={tradeItemStyle.tradeInfoBox}>
+        <Badge type="나" />
+        <Text style={tradeItemStyle.tradeItemText}>{props.card1}</Text>
+      </View>
+      <View style={tradeItemStyle.tradeInfoBox}>
+        <Badge type="당신" />
+        <Text style={tradeItemStyle.tradeItemText}>{props.card2}</Text>
+      </View>
+    </Pressable>
+  );
 }
---
+++ components/trade-item/style.ts
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+export const tradeItemStyle = StyleSheet.create({
+  basic: {
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 4,
+    display: 'flex',
+    boxSizing: 'border-box',
+    backgroundColor: '#ffffff',
+    alignItems: 'flex-start',
+    gap: 6,
+    width: '100%',
+  },
+  titleBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    boxSizing: 'border-box',
+    marginBottom: 4,
+    width: '100%',
+  },
+  title: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: 700,
+    color: '#373737',
+    marginRight: 'auto',
+  },
+  tradeInfoBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    boxSizing: 'border-box',
+    justifyContent: 'flex-start',
+    gap: 8,
+  },
+  tradeItemText: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: 400,
+    color: '#373737',
+  },
+});
