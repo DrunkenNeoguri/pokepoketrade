5c798d203316d7d719b1337e60f179b11e75440b
developneoguri <developneoguri@gmail.com>
2025-04-02 00:16:50 +0900

    fix: Bottom Tab에 우선적으로 SVG 컴포넌트 배치

--- components/bottom-tab/index.tsx
+++ components/bottom-tab/index.tsx
@@ -6,33 +6,44 @@ import {
   SearchScreen,
   TradeScreen,
 } from '../../screens';
+import {
+  ListIcon,
+  MyInfoIcon,
+  RegisterIcon,
+  SearchIcon,
+  TradeIcon,
+} from '../../assets/svgs';
 
 const Tab = createBottomTabNavigator();
 
 export function BottomTab() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="메인" component={MainScreen} />
+      <Tab.Screen
+        name="메인"
+        component={MainScreen}
+        options={{headerShown: true, tabBarIcon: () => <ListIcon />}}
+      />
       <Tab.Screen
         name="검색"
         component={SearchScreen}
-        options={{headerShown: true}}
+        options={{headerShown: true, tabBarIcon: () => <SearchIcon />}}
       />
       <Tab.Screen
         name="등록"
         component={RegisterScreen}
-        options={{headerShown: true}}
+        options={{headerShown: true, tabBarIcon: () => <RegisterIcon />}}
       />
 
       <Tab.Screen
         name="트레이드"
         component={TradeScreen}
-        options={{headerShown: true}}
+        options={{headerShown: true, tabBarIcon: () => <TradeIcon />}}
       />
       <Tab.Screen
         name="내 정보"
         component={MyInfoScreen}
-        options={{headerShown: true}}
+        options={{headerShown: true, tabBarIcon: () => <MyInfoIcon />}}
       />
     </Tab.Navigator>
   );
