cd1b541c1464a0abc4539b782a1e839303e99ab6
developneoguri <developneoguri@gmail.com>
2025-04-02 00:16:28 +0900

    feat: 프로젝트에 사용한 svg 파일 추가 및 컴포넌트화

---
+++ assets/svgs/index.ts
@@ -0,0 +1,8 @@
+import ListIcon from './icon-list.svg';
+import MyInfoIcon from './icon-myinfo.svg';
+import RegisterIcon from './icon-register.svg';
+import SearchIcon from './icon-search.svg';
+import TradeIcon from './icon-trade.svg';
+import CloseIcon from './icon-close.svg';
+
+export {ListIcon, MyInfoIcon, RegisterIcon, SearchIcon, TradeIcon, CloseIcon};
