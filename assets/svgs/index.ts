f0fd6afd9a515fab33327b2dff5cf5073377ecae
developneoguri <developneoguri@gmail.com>
2025-04-15 22:18:08 +0900

    fix: 아이콘 파일 잘못된 사항들 수정, 일부 아이콘명 수정

--- assets/svgs/index.ts
+++ assets/svgs/index.ts
@@ -1,5 +1,5 @@
 import ListIcon from './icon-list.svg';
-import MyInfoIcon from './icon-myinfo.svg';
+import MyInfoIcon from './icon-my-info.svg';
 import RegisterIcon from './icon-register.svg';
 import SearchIcon from './icon-search.svg';
 import TradeIcon from './icon-trade.svg';
